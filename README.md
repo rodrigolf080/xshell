ccffa76f5fa3ffbbb434dd4552b8697dd7cb68fe
rodrigolf080 <rodrigolf080@gmail.com>
2021-03-20 14:32:04 +0000

    garbage code

--- README.md
+++ README.md
@@ -1,18 +1,13 @@
-# xshell
-----
--- Disclaimer:
-:: This program is a modified work from multiple scources listed below under the next section
-:: Please read the "LICENSE" for more information
+# myshell
 ----
 -- Sources:
-:: Website Article :: www.geeksforgeeks.org :: https://www.geeksforgeeks.org/making-linux-shell-c/, thank you to Suprotik Dey (LinkedIn :: https://www.linkedin.com/in/suprotik-dey-368706127/)
 :: Website :: https://theasciicode.com.ar/extended-ascii-code/box-drawings-single-line-vertical-right-character-ascii-code-195.html
-:: Blog Article :: https://codeforwin.org/2018/03/c-program-to-list-all-files-in-a-directory-recursively.html, thank you to Pankaj (Twitter :: https://twitter.com/pankajprakashh)
 :: GitHub Page :: https://siongui.github.io/2013/01/09/c-remove-string-trailing-newline-carriage-return/
+:: Website :: codeforwin.org :: Articles and Resources
 :: Website :: www.geeksforgeeks.org :: Articles and Resources
 ----
 -- Description:
-:: "xshell" or "xsh" is an Open Source Unix Shell built in C by forking a child process to execute the job, using GNU libreadline-dev, execvp system call for non built in commands and allows piping and background processes.
+:: myshell is a Unix Sihell built in C by forking a child process to execute the job, using GNU libreadline-dev, execvp system call for non built in commands and allows piping and background processes.
 ----
 -- Dependencies:
 :: GNU Readline libreadline-dev package
@@ -24,9 +19,9 @@
 :: $ ./run
 ----
 -- Notes:
-:: "run" is a bash script that compiles the program using gcc and a makefile, clears the screen and executes the binary lauching the shell
-:: File "dev" holds a running script to catch compilation errors for development
-:: Error list
+:: Background processing should be achievied by allowing the user to interact with the shell stdin and stdout (and stderr) while the child process is executing and when its done bringing the  stdout and stderr to the foreground
+:: IO redirection should be achieved by parsing user input and duplicating the file descriptors for stdin and/or stdin accordingly to the files following the read write append (<, >, >>) 
+:: Error list for exec:
 :: :: 0 -- failed to cast execvp on arguments
 :: :: 1 -- failed to cast execvp on piped arguments at the write end
 :: :: 2 -- failed to cast execvp at the read end
@@ -36,3 +31,5 @@
 :: cd :: C function to change directory chdir()
 :: help :: displays the list of commands and functions
 :: clear :: prints a string to clear the screen
+:: dir :: print files and directories
+:: print :: print a comment
--- src/utils.c
+++ src/utils.c
@@ -1,12 +1,13 @@
-#include <stdio.h> 
-#include <string.h> 
-#include <stdlib.h> 
-#include <unistd.h> 
-#include <sys/types.h> 
-#include <sys/wait.h> 
-#include <readline/readline.h> 
-#include <readline/history.h> 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 #include <dirent.h>
+#include <fcntl.h> 
 // macros
 #include "macros.h"
 
@@ -15,19 +16,20 @@
  *
  * @param :: void
  */
-void init_shell() 
-{ 
-    clear(); 
+void init_shell()
+{
+    clear();
     printf("\n\n\n\n\t******************"
-        "************************"); 
-    printf("\n\n\n\t            ***** XSH *****       "); 
-    printf("\n\n\t  ***** Open Source Unix Shell *****"); 
+           "************************");
+    printf("\n\n\n\t            ***** XSH *****       ");
+    printf("\n\n\t  ***** Open Source Unix Shell *****");
     printf("\n\n\n\n\t*******************"
-        "***********************"); 
-    printf("\n"); 
-    sleep(3); 
-    clear(); 
-} 
+           "***********************");
+    printf("\n");
+    sleep(3);
+    clear();
+}
+
 
 /*
  * Take input string
@@ -35,12 +37,13 @@ void init_shell()
  * Apply a null byte terminator
  * @param :: str to be format
  */
-void stringNullTerminate(char *str) {
-  if (str == NULL)
-    return;
-  int length = strlen(str);
-  if (str[length-1] == '\n')
-    str[length-1]  = '\0';
+void stringNullTerminate(char *str)
+{
+    if (str == NULL)
+        return;
+    int length = strlen(str);
+    if (str[length - 1] == '\n')
+        str[length - 1] = '\0';
 }
 
 /*
@@ -48,19 +51,22 @@ void stringNullTerminate(char *str) {
  * Return the initial str with the user input in it
  * @param :: empty *str
  */
-int takeInput(char *str) 
-{ 
-    char* buf; 
-  
-    buf = readline("$ "); 
-    if (strlen(buf) != 0) { 
-        add_history(buf); 
-        strcpy(str, buf); 
-        return 0; 
-    } else { 
-        return 1; 
-    } 
-} 
+int takeInput(char *str)
+{
+    char *buf;
+
+    buf = readline("$ ");
+    if (strlen(buf) != 0)
+    {
+        add_history(buf);
+        strcpy(str, buf);
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
+}
 
 /*
  * Get env var $USER (name)
@@ -70,15 +76,15 @@ int takeInput(char *str)
  * @param :: void
  * @return :: *str with user input 
  */
-void printDir() 
-{ 
-    char cwd[1024]; 
-    char *username = getenv("USER");    
-    getcwd(cwd, sizeof(cwd)); 
+void printDir()
+{
+    char cwd[1024];
+    char *username = getenv("USER");
+    getcwd(cwd, sizeof(cwd));
     printf("%s", username);
     printf("@");
-    printf("%s", cwd); 
-} 
+    printf("%s", cwd);
+}
 
 /*
  * Tree directory viewer - prints all files and sub-directories of a given 
@@ -104,13 +110,12 @@ void tree(char *basePath, const int root)
         if (strcmp(dp->d_name, ".") != 0 && strcmp(dp->d_name, "..") != 0)
         {
             // manage depth on tree branches
-            for (i=0; i<root; i++) 
+            for (i = 0; i < root; i++)
             {
-                if (i%2 == 0 || i == 0)
+                if (i % 2 == 0 || i == 0)
                     printf("--::");
                 else
                     printf("");
-
             }
             // list contents of current directory as "root" path
             printf("--::%s\n", dp->d_name);
@@ -125,37 +130,42 @@ void tree(char *basePath, const int root)
     closedir(dir);
 }
 
-
 /*
  * Forks parent process and executes command on child then kills child process
  * Use C System lib execvp for system command execution
  * 
  * @param parsed :: first parsed command
  */
-void execArgs(char **parsed) 
-{ 
-    // Forking a child 
-    pid_t pid = fork();  
-    
+void execArgs(char **parsed)
+{
+    // Forking a child
+    pid_t pid = fork();
     // Parent returns >1
     // Child success returns 0
     // Forking failure returns <1
 
-    if (pid == -1) { 
-        printf("\nFailed forking child"); 
-        return; 
-    } else if (pid == 0) { 
-        if (execvp(parsed[0], parsed) < 0) { 
-            printf("\nCould not execute command error: 0\n"); 
-        } 
-        exit(0); 
-    } else { 
-        // Waiting for child to terminate 
-        wait(NULL);  
-        return; 
-    } 
-} 
 
+    if (pid == -1)
+    {
+        printf("\nFailed forking child");
+        return;
+    }
+    else if (pid == 0)
+    {
+        if (execvp(parsed[0], parsed) < 0)
+        {
+            printf("\nCould not execute command error: 0\n");
+        }
+        exit(0);
+    }
+    else
+    {
+        // Waiting for child to terminate
+
+        wait(NULL);
+        return;
+    }
+}
 
 /*
  * Forks parent process and executes first command
@@ -166,150 +176,172 @@ void execArgs(char **parsed)
  * @param parsed :: first parsed command
  * @param parsed :: second parsed command
  */
-void execArgsPiped(char **parsed, char **parsedpipe) 
-{ 
-    // pipes :: 0 is read end, 1 is write end 
-    int pipefd[2];  
-    pid_t p1, p2; 
-  
-  	// return -1 on failure
-    if (pipe(pipefd) < 0) { 
-        printf("\nPipe could not be initialized\n"); 
-        return; 
-    } 
+void execArgsPiped(char **parsed, char **parsedpipe)
+{
+    // pipes :: 0 is read end, 1 is write end
+    int pipefd[2];
+    pid_t p1, p2;
+
+    // return -1 on failure
+    if (pipe(pipefd) < 0)
+    {
+        printf("\nPipe could not be initialized\n");
+        return;
+    }
     // return child :: 0 success
     // return parent :: 1 success -1 failure
-    p1 = fork(); 
-    if (p1 < 0) { 
-        printf("\nCould not fork\n"); 
-        return; 
-    } 
-  
-    // 0 executes on child and >1 on parent
-    if (p1 == 0) { 
-        // Child 1 executing.. 
-        // It only needs to write at the write end 
-        close(pipefd[0]); 
-        dup2(pipefd[1], STDOUT_FILENO); 
-        close(pipefd[1]); 
-  
-        if (execvp(parsed[0], parsed) < 0) { 
-            printf("\nCould not execute command error: 1\n"); 
-            exit(0); 
-        } 
-    } else { 
-        // Parent executing 
-        p2 = fork(); 
-  
-        if (p2 < 0) { 
-            printf("\nCould not fork\n"); 
-            return; 
-        } 
-  
-        // Child 2 executing.. 
-        // It only needs to read at the read end 
-        if (p2 == 0) { 
-            close(pipefd[1]); 
-            dup2(pipefd[0], STDIN_FILENO); 
-            close(pipefd[0]); 
-            if (execvp(parsedpipe[0], parsedpipe) < 0) { 
-                printf("\nCould not execute command error: 2..\n"); 
-                exit(0); 
-            } 
-        } else { 
-            // parent executing, waiting for last child
-            wait(NULL); 
-        } 
-    } 
-} 
+    p1 = fork();
+    if (p1 < 0)
+    {
+        printf("\nCould not fork\n");
+        return;
+    }
 
+    // 0 executes on child and >1 on parent
+    if (p1 == 0)
+    {
+        // Child 1 executing..
+        // It only needs to write at the write end
+        close(pipefd[0]);
+        dup2(pipefd[1], STDOUT_FILENO);
+        close(pipefd[1]);
+
+        if (execvp(parsed[0], parsed) < 0)
+        {
+            printf("\nCould not execute command error: 1\n");
+            exit(0);
+        }
+    }
+    else
+    {
+        // Parent executing
+        p2 = fork();
+
+        if (p2 < 0)
+        {
+            printf("\nCould not fork\n");
+            return;
+        }
+
+        // Child 2 executing..
+        // It only needs to read at the read end
+        if (p2 == 0)
+        {
+            close(pipefd[1]);
+            dup2(pipefd[0], STDIN_FILENO);
+            close(pipefd[0]);
+            if (execvp(parsedpipe[0], parsedpipe) < 0)
+            {
+                printf("\nCould not execute command error: 2..\n");
+                exit(0);
+            }
+        }
+        else
+        {
+            // parent executing, waiting for last child
+            wait(NULL);
+        }
+    }
+}
 
 /*
  * Display help section with available commands and features
  *
  * @param :: void
  */
-void openHelp() 
-{ 
+void openHelp()
+{
     puts("\n***WELCOME TO XSH HELP***"
-        "\nList of Commands supported:"
-        "\n>cls - clear screen"
-        "\n>cd - change directory"
-        "\n>quit - quit shell"
-        "\n>dir - list files recursively starting"
-        " on the current directory in a tree-liek structure"
-        "\n>all other general system commands available in UNIX shell"
-        "\n>pipe handling"
-        "\n>improper space handling"
-        "\n>script execution\n"); 
+         "\nList of Commands supported:"
+         "\n>cls - clear screen"
+         "\n>cd - change directory"
+         "\n>quit - quit shell"
+         "\n>dir - list files recursively starting"
+         " on the current directory in a tree-liek structure"
+         "\n>all other general system commands available in UNIX shell"
+         "\n>pipe handling"
+         "\n>improper space handling"
+         "\n>script execution\n");
 
-    return; 
-} 
+    return;
+}
 
 /*
  * Execute builtin commands
  *
  * @param :: void
  */
-int ownCmdHandler(char **parsed) 
-{ 
+int ownCmdHandler(char **parsed)
+{
     // Build an array of available commands to use
     // When user input matches this commands cast the according switch case
     // With command functionality
 
-    int NoOfOwnCmds = 6, i, switchOwnArg = 0; 
-    char* ListOfOwnCmds[NoOfOwnCmds]; 
-    char* username;
-  
-    ListOfOwnCmds[0] = "quit"; 
-    ListOfOwnCmds[1] = "cd"; 
-    ListOfOwnCmds[2] = "help"; 
+    int NoOfOwnCmds = 7, i, switchOwnArg = 0;
+    char *ListOfOwnCmds[NoOfOwnCmds];
+    char *username;
+
+    ListOfOwnCmds[0] = "quit";
+    ListOfOwnCmds[1] = "cd";
+    ListOfOwnCmds[2] = "help";
     ListOfOwnCmds[3] = "hello";
-    ListOfOwnCmds[4] = "cls"; 
+    ListOfOwnCmds[4] = "cls";
     ListOfOwnCmds[5] = "dir";
-  
-    for (i = 0; i < NoOfOwnCmds; i++) { 
+    ListOfOwnCmds[6] = "print";
+
+    for (i = 0; i < NoOfOwnCmds; i++)
+    {
         // strcmp returns 0 in case of match
         // we make the flag have the same value of the according switch statemnt
-        if (strcmp(parsed[0], ListOfOwnCmds[i]) == 0) { 
-            switchOwnArg = i + 1; 
-            break; 
-        } 
-    } 
-  
-    switch (switchOwnArg) { 
-    case 1: 
+        if (strcmp(parsed[0], ListOfOwnCmds[i]) == 0)
+        {
+            switchOwnArg = i + 1;
+            break;
+        }
+    }
+
+    switch (switchOwnArg)
+    {
+    case 1:
         printf("\nGoodbye!\n");
         sleep(1);
-        clear(); 
-        exit(0); 
-    case 2: 
-    	if (chdir(parsed[1]) == 0) {
-    	} else {
-    		printf("\nPath not found.\n");
-    	}
-        return 1; 
-    case 3: 
-        openHelp(); 
-        return 1; 
-    case 4: 
-        username = getenv("USER"); 
+        clear();
+        exit(0);
+    case 2:
+        if (chdir(parsed[1]) == 0)
+        {
+        }
+        else
+        {
+            printf("\nPath not found.\n");
+        }
+        return 1;
+    case 3:
+        openHelp();
+        return 1;
+    case 4:
+        username = getenv("USER");
         printf("\nHello %s!"
-            "\nUse help to know more..\n", 
-            username); 
+               "\nUse help to know more..\n",
+               username);
         return 1;
     case 5:
         clear();
         return 1;
     case 6:
-    	tree(".", 0);
+        tree(".", 0);
         return 1;
-    default: 
-        break; 
-    } 
-  
-    return 0; 
-} 
+    case 7:
+        printf("%s\n", parsed[0]);
+        return 1;
+    case 8:
+
+    default:
+        break;
+    }
+
+    return 0;
+}
 
 /*
  * Look for pipe in a string
@@ -318,23 +350,23 @@ int ownCmdHandler(char **parsed)
  * @param str :: command string before pipe
  * @param strpiped :: command array after pipe is found
  */
-int parsePipe(char *str, char **strpiped) 
-{ 
-    int i; 
-    for (i = 0; i < 2; i++) { 
-        strpiped[i] = strsep(&str, "|"); 
-        if (strpiped[i] == NULL) 
-            break; 
-    } 
-  
-    if (strpiped[1] == NULL) 
-        return 0; // returns zero if no pipe is found. 
-    else { 
-        return 1; 
-    } 
-} 
+int parsePipe(char *str, char **strpiped)
+{
+    int i;
+    for (i = 0; i < 2; i++)
+    {
+        strpiped[i] = strsep(&str, "|");
+        if (strpiped[i] == NULL)
+            break;
+    }
 
-
+    if (strpiped[1] == NULL)
+        return 0; // returns zero if no pipe is found.
+    else
+    {
+        return 1;
+    }
+}
 
 /*
  * Look for spaces in input string
@@ -344,20 +376,22 @@ int parsePipe(char *str, char **strpiped)
  * @param str :: string of commands to parse
  * @param parsed :: array of commands
  */
-void parseSpace(char* str, char** parsed) 
-{ 
-    int i; 
-  
+void parseSpace(char *str, char **parsed)
+{
+    int i;
+
     // remove space and go back on the array to manage empty space
-    for (i = 0; i < MAXLIST; i++) { 
-        parsed[i] = strsep(&str, " "); 
-  
-        if (parsed[i] == NULL) 
-            break; 
-        if (strlen(parsed[i]) == 0) 
-            i--; 
-    } 
-} 
+    for (i = 0; i < MAXLIST; i++)
+    {
+        parsed[i] = strsep(&str, " ");
+
+        if (parsed[i] == NULL)
+            break;
+        if (strlen(parsed[i]) == 0)
+            //parsed[i] = "\0";
+            i--;
+    }
+}
 
 /*
  * Handle Piping and IO redirection
@@ -368,30 +402,31 @@ void parseSpace(char* str, char** parsed)
  * @param parsed :: array of commands
  * @param parsed :: array of commands
  */
-int processString(char *str, char **parsed, char **parsedpipe) 
-{ 
-  
-    char* strpiped[2];
+int processString(char *str, char **parsed, char **parsedpipe)
+{
 
-    int piped = 0; 
-  
+    char *strpiped[2];
+
+    int piped = 0;
+
     piped = parsePipe(str, strpiped);
- 
 
-    if (piped) { 
-        parseSpace(strpiped[0], parsed); 
-        parseSpace(strpiped[1], parsedpipe); 
-  
-    } else {
-    		parseSpace(str, parsed); 
+    if (piped)
+    {
+        parseSpace(strpiped[0], parsed);
+        parseSpace(strpiped[1], parsedpipe);
     } 
-  
-    if (ownCmdHandler(parsed))
-        return 0; 
     else
-    	// returns 1 on simple command and 2 on piped command
-        return 1 + piped; 
-} 
+    {
+        parseSpace(str, parsed);
+    }
+
+    if (ownCmdHandler(parsed))
+        return 0;
+    else
+        // returns 1 on simple command and 2 on piped
+        return 1 + piped;
+}
 
 /*
  * Process string into an array of commands
@@ -401,26 +436,27 @@ int processString(char *str, char **parsed, char **parsedpipe)
  */
 void execParsedArgs(char *inputString)
 {
-    char *parsedArgs[MAXLIST]; 
+    char *parsedArgs[MAXLIST];
     char *parsedArgsPiped[MAXLIST];
 
     int execFlag = 0;
-    // process 
-    execFlag = processString(inputString, 
-    parsedArgs, parsedArgsPiped);
-    // execflag returns zero if there is no command 
-    // or it is a builtin command, 
-    // 1 if it is a simple command 
-    // 2 if it is including a pipe. 
+    // process
+    execFlag = processString(inputString,
+                             parsedArgs, parsedArgsPiped);
+    // execflag returns zero if there is no command
+    // or it is a builtin command,
+    // 1 if it is a simple command
+    // 2 if it is including a pipe.
 
-    // execute 
-    if (execFlag == 1) {
-        execArgs(parsedArgs); 
+    // execute
+    if (execFlag == 1)
+    {
+        execArgs(parsedArgs);
     }
-    if (execFlag == 2) {
+    if (execFlag == 2)
+    {
         execArgsPiped(parsedArgs, parsedArgsPiped);
     }
-
 }
 
 /*
@@ -434,25 +470,41 @@ void execScript(char **script, char *inputString)
 {
     FILE *fp;
 
-    if ((fp = fopen(*script, "r")) == NULL){
+    if ((fp = fopen(*script, "r")) == NULL)
+    {
         printf("File >>> %s <<< not found.\n", *script);
         printf("Quitting...\n\n");
         exit(0);
     }
-    else {
+    else
+    {
         // open file and parse commands
         printf("Executing from file...\n");
         // need to go over each line
         // store each line
         // execparsedargs of each line
-        while (fgets(inputString, MAXCOM,fp) != NULL) {
-            stringNullTerminate(inputString);
-            execParsedArgs(inputString);
+        int id = fork();
+        if (id == -1)
+        {
+            printf("Problem forking...\n");
         }
+        else if (id == 0)
+        {
+            while (fgets(inputString, MAXCOM, fp) != NULL)
+            {
+                stringNullTerminate(inputString);
+                execParsedArgs(inputString);
+            }
+        }
+        else
+        {
+            wait(NULL);
+        }
+
         //fgets(inputString, MAXCOM, fp);
         //printf( "%s" , inputString);
 
         fclose(fp);
         exit(0);
-    }   
+    }
 }
